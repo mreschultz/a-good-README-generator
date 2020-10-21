4ce59a2692169c3a1fcfbfdef5036339c89d6fb9
Eric Schultz <mreschultz@me.com>
2020-10-21 15:02:27 -0400

    fixed index file

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
-const inquirer = require("inquire");
+const inquirer = require("inquirer");
 const api = "./api";
 const generateMarkdown = require("./generateMarkdown");
 
 // array of questions for user
 const questions = [
 	{
-		type: Input,
-		name: github,
+		type: "input",
+		name: "github",
 		message: "What's your GitHub name?",
 	},
 	{
@@ -19,7 +19,7 @@ const questions = [
 		type: "list",
 		name: "license",
 		message: "What kind of license do you use?",
-		choices: ["MIT", "None"]
+		choices: ["MIT", "None"],
 	},
 ];
 
@@ -29,7 +29,7 @@ function writeToFile(fileName, data) {}
 // function to initialize program
 function init() {}
 inquirer.prompt(questions).then((response) => {
-	console.log(reponse);
+	console.log(response);
 });
 // function call to initialize program
 init();
